88750f5afc0ef93306e09a6c8caefbbbba01f5ab
Robs <robertmichaellim@gmail.com>
2017-01-23 02:57:20 +0800

    finished POT.c

--- Priority Queues/POT.c
+++ Priority Queues/POT.c
@@ -24,6 +24,7 @@ void display(Set A);
 void insertMin(Set *A, int x);
 void heapifyMin(Set *A);
 int deleteMin(Set *A);
+void heapSortMax(Set *P);
 
 int main()
 {
@@ -36,13 +37,13 @@ int main()
 	for(y = 0;y < x;y++){
 		printf("Element no.%d: ",y+1);
 		scanf("%d",&z);
-		insert(&A,z);
+		insertMin(&A,z);
 	}
 	printf("After Insertion: ");display(A);
 	heapifyMin(&A);
 	printf("After Min Heapification: ");display(A);
-	printf("After deleteMin: ");deleteMin(&A);
-	display(A);
+	heapSortMax(&A);
+	printf("After heapsortMax: ");display(A);
 	return 0;
 }
 
@@ -68,10 +69,9 @@ void insertMin(Set *P, int x)
 	if(P->last < MAX-1){
 		cPos = ++P->last;
 		P->elem[cPos] = x;
-		for(pPos = (cPos-1)/2;pPos >= 0 && P->elem[cPos] < P->elem[pPos];pPos = (cPos-1)/2){
+		for(pPos = (cPos-1)/2;pPos >= 0 && P->elem[cPos] < P->elem[pPos];cPos = pPos, pPos = (cPos-1)/2){
 			P->elem[cPos] = P->elem[pPos];
 			P->elem[pPos] = x;
-			cPos = pPos;
 			
 		}
 	}
@@ -87,7 +87,7 @@ void heapifyMin(Set *A)
 		leftChild = (parentPos*2)+1;
 		rightChild = leftChild +1;
 		
-		while(leftChild < A->last && (top > A->elem[leftChild] || top >  A->elem[rightChild])){
+		while(leftChild < A->last && (top > A->elem[leftChild] ||  top > A->elem[rightChild])){
 			temp = (A->elem[leftChild] < A->elem[rightChild])? leftChild: rightChild;
 			
 			A->elem[parentPos] = A->elem[temp];
@@ -101,23 +101,36 @@ void heapifyMin(Set *A)
 
 int deleteMin(Set *A)
 {
-	int parentPos, leftChild, rightChild, temp, top;
+	int parentPos, leftChild, rightChild, temp, top, retval;
+	retval = A->elem[0];
 	if(A->last > 0){
 		top = A->elem[0] = A->elem[A->last--];
 		parentPos = 0;
 		leftChild = 1;
 		rightChild = 2;
 		
-		while(leftChild <= A->last && (top > A->elem[leftChild] || top > A->elem[rightChild])){
-			temp = (A->elem[leftChild] < A->elem[rightChild])?leftChild:rightChild;
+		while(leftChild <= A->last && (top > A->elem[leftChild] ||(rightChild > A->last|| top > A->elem[rightChild]))){
+			temp = (rightChild > A->last || A->elem[leftChild] < A->elem[rightChild])?leftChild:rightChild;
 			A->elem[parentPos] = A->elem[temp];
 			A->elem[temp] = top;
 			parentPos = temp;
 			leftChild = (parentPos*2)+1;
 			rightChild = leftChild +1; 
 		}
+	}else if(A->last == 0){
+		A->last--;
 	}
-	
+	return retval;
+}
+
+void heapSortMax(Set *P)
+{
+	int oldLast,x;
+	oldLast = P->last;
+	for(x = 0;x < oldLast;x++){
+		P->elem[oldLast-x] = deleteMin(P);
+	}
+	P->last = oldLast;
 }
 
 void display(Set A)
